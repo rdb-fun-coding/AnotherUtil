8997727a23dcf081a7b55cf94abf7d0f87258a32
brah-git <rahuldeb.barman@ibm.com>
2026-02-09 17:06:53 +0530

    adding logging statement

--- code/source/utils.java
+++ code/source/utils.java
@@ -7,8 +7,10 @@ import com.wm.util.Values;
 import com.wm.app.b2b.server.Service;
 import com.wm.app.b2b.server.ServiceException;
 // --- <<IS-START-IMPORTS>> ---
+import com.wm.lang.ns.NSName;
 import com.wm.passman.PasswordManagerException;
 import com.wm.util.security.WmSecureString;
+import java.util.logging.Level;
 import com.wm.app.b2b.server.OutboundPasswordManager;
 // --- <<IS-END-IMPORTS>> ---
 
@@ -43,19 +45,30 @@ public final class utils
 		String key = ValuesEmulator.getString(pipeline, "key");
 		String inHandle = "com.webmethods.cloudstream." + alias + "." + key;
 		System.out.println("inHandle: " + inHandle);
+		
+		String serviceToLog = "wm.cloudstreams.service.util.runtime:log";
+		NSName serviceNsName = NSName.create(serviceToLog);
+		IData logPipeline = IDataFactory.create();
+		ValuesEmulator.put(logPipeline, "connectorID", "DUMMY_CONN_ID");
+		ValuesEmulator.put(logPipeline, "severityLevel", Level.INFO);
 		try {
 			WmSecureString retrievePassword = OutboundPasswordManager.getPasswordManager(pkgName).retrievePassword(inHandle);
 			if(retrievePassword == null) {
 				System.out.println("Got no secret with OutboundPasswordManager.getPasswordManager(pkgName).retrievePassword(inHandle) and pacakge as : " + pkgName);
+				ValuesEmulator.put(logPipeline, "message", "Could not retrieve any password for handle : " + inHandle);
 				ValuesEmulator.put(pipeline, "secret", "NO_SECRET_RETRIEVED");
 			}
-			if(retrievePassword != null){
+			else if(retrievePassword != null){
 				String secret = retrievePassword.toString();
 				System.out.println("Got this secret with OutboundPasswordManager.getPasswordManager(pkgName).retrievePassword(inHandle) : " + secret);
+				ValuesEmulator.put(logPipeline, "message", "Got this secret: " + secret + " with handle: " + inHandle);
 				ValuesEmulator.put(pipeline, "secret", secret);
 			}
+			Service.doInvoke(serviceNsName, logPipeline); 
 		}
 		catch (PasswordManagerException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		// --- <<IS-END>> ---
@@ -79,19 +92,29 @@ public final class utils
 		String key = ValuesEmulator.getString(pipeline, "key");
 		String inHandle = "com.webmethods.cloudstream." + alias + "." + key;
 		System.out.println("inHandle: " + inHandle);
+		String serviceToLog = "wm.cloudstreams.service.util.runtime:log";
+		NSName serviceNsName = NSName.create(serviceToLog);
+		IData logPipeline = IDataFactory.create();
+		ValuesEmulator.put(logPipeline, "connectorID", "DUMMY_CONN_ID");
+		ValuesEmulator.put(logPipeline, "severityLevel", Level.INFO);
 		try {
 			WmSecureString retrievePassword = OutboundPasswordManager.retrievePassword(pkgName, inHandle);
 			if(retrievePassword == null) {
 				System.out.println("Got no result with pacakge as : " + pkgName);
+				ValuesEmulator.put(logPipeline, "message", "Could not retrieve any password for handle : " + inHandle);
 				retrievePassword = OutboundPasswordManager.retrievePassword(null, inHandle);
 			}
-			if(retrievePassword != null){
+			else if(retrievePassword != null){
 				String secret = retrievePassword.toString();
 				System.out.println("Got this secret: " + secret);
+				ValuesEmulator.put(logPipeline, "message", "Got this secret: " + secret + " with handle: " + inHandle);
 				ValuesEmulator.put(pipeline, "secret", secret);
 			}
+			Service.doInvoke(serviceNsName, logPipeline); 
 		}
 		catch (PasswordManagerException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		// --- <<IS-END>> ---
